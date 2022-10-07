a0323925d92012eedf15fc0edbe01cd7d2b318e7
Matt Palmer <matt@enquo.org>
2022-10-07 11:27:54 +1100

    Avoid potential crashes due to key data getting GC'd

--- ruby/lib/enquo/root.rb
+++ ruby/lib/enquo/root.rb
@@ -6,6 +6,10 @@ module Enquo
 				_new_from_static_root_key(key)
 			else
 				raise ArgumentError, "key must be a root key provider object (got a #{key.class})"
+			end.tap do |k|
+				# DIRTY HACK ALERT: take a reference to the key so it doesn't get GC'd
+				# If someone can come up with a better way to acheive this, I'm all ears
+				k.instance_variable_set(:@_key, key)
 			end
 		end
 
